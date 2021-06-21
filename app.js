ae39b9f051c51959e3697e50dad6122b22320789
unknown <xlanimation@gmail.com>
2021-06-21 14:29:41 -0500

    added notes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ $(document).ready(() => {
         console.log(`Yooooo`);
     }
 
-    // rollDice funtion. Loops through the dieOnScreen object and re-rolls the dice that are displayed once the roll button is clicked. 
+    // rollDice function. Loops through the dieOnScreen object and re-rolls the dice that are displayed once the roll button is clicked. 
     const rollDice = () => {
         // Loop through the dieOnScreen object.
         for (let i = 0; i < dieOnScreen.length; i++) {
@@ -25,25 +25,36 @@ $(document).ready(() => {
             dieOnScreen[i].roll();
         }
     };
-
+    
+    // checkSum function. 
     const checkSum = () => {
+        // Set sum at 0
         let sum = 0;
+        // Loop through dieOnScreen object and get sum of values. 
         for (let i = 0; i < dieOnScreen.length; i++) {
             sum += dieOnScreen[i].value;
         }
+        // output sum in an alert.
         alert(`The value sum of all dice on the screen is ${sum}.`);
     };
 
+    // Calls.
     generateBtn.click(addDie);
     rollBtn.click(rollDice);
     sumBtn.click(checkSum);
 
+    // Die class.
     class Die {
+        // can accept values. none are being passed so none is needed.
         constructor() {
+            // create new div and assign class.
             this.div = $('<div></div>');
             this.div.attr(`class`, `die`);
+            // rolls the die (this) object.
             this.roll()
+            // add new div to divContainer.
             $('#dieContainerDiv').append(this.div);
+            // add new div object to dieOnScreen object holder.
             dieOnScreen.push(this);
 
             // anon function. re-rolls the dice once dice div is clicked.
@@ -51,6 +62,7 @@ $(document).ready(() => {
                 this.roll();
             });
 
+            // anon function. removes die div and die object ****not working*****
             $(this.div).dblclick(() => {
                 $(this.div).remove();
                 dieOnScreen.splice($.inArray(this, dieOnScreen),1);
@@ -58,13 +70,17 @@ $(document).ready(() => {
             });
         }
 
+        // roll function.
         roll() { 
+            // get a random number between 1-6
             this.value = Math.floor(Math.random() * (6 - 1) + 1);
+            // calls getChar function.
             this.getChar();
+            // adds new character to div.
             $(this.div).text("" + this.char + "");
-            $(this.div).addClass("die");
         }
 
+        // getChar function. takes random generated value checks it and replaces it with die char.
         getChar() {
             if (this.value === 1) {
                 this.char = "⚀";
@@ -83,12 +99,5 @@ $(document).ready(() => {
     }
 
 });
-
-
-
-
-
-// let newDie1 = new Die();
-// newDie1.dieGenerator();
 
 console.log('%cStart%c of scripts.js file!', 'font-weight: 900; color: green;', 'font-weight: 400; color: green;');
